7c5e5f36455d3729d9e495f8cafaf85ba009fa01
이윤세 <vahyeon2506@sonline20.sen.go.kr>
2025-04-04 14:31:10 +0900

    Drift 연기 추가

--- Assets/02_Scripts/Drift.cs
+++ Assets/02_Scripts/Drift.cs
@@ -4,18 +4,19 @@ public class Drift : MonoBehaviour
 {
     [SerializeField] float accleration = 20.0f;         // 전진 / 후진 가속도
     [SerializeField] float steering = 3.0f;               // 조항 속도
-    [SerializeField] float maxSpeed = 10.0f;
+    [SerializeField] float maxSpeed = 30.0f;
     [SerializeField] float driftFactor = 0.95f;         // 낮을 수록 더 미끄러짐
+
+    // 이펙트
+    [SerializeField] ParticleSystem smokeLeft;
+    [SerializeField] ParticleSystem smokeRight;
+
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     } 
-    void Update()
-    {
-        
-    }
     void FixedUpdate()
     {
         float speed = Vector2.Dot(rb.linearVelocity, transform.up);
@@ -31,6 +32,25 @@ public class Drift : MonoBehaviour
         Vector2 fowardVelocity = transform.up * Vector2.Dot(rb.linearVelocity, transform.up);
         Vector2 sideVelocity = transform.right * Vector2.Dot(rb.linearVelocity,transform.right);
         rb.linearVelocity = fowardVelocity + sideVelocity*driftFactor;    
+
+    }
+
+    void Update()
+    {
+        float sidewayVelocity= Vector2.Dot(rb.linearVelocity, transform.right);
+
+        bool isDriftting = rb.linearVelocity.magnitude > 2.0f && Mathf.Abs(sidewayVelocity) > 1.0f;
+        if(isDriftting)
+        {
+            if(!smokeLeft.isPlaying) smokeLeft.Play();
+            if(!smokeRight.isPlaying) smokeRight.Play();
+        }
+        else
+        {
+            if(smokeLeft.isPlaying) smokeLeft.Stop();
+            if(smokeRight.isPlaying) smokeRight.Stop();
+        }
+
 
     }
 }
